ea7b30f6741de910eef0cd7fdede159b52257a78
KrisH8511762699 <146726398+KrisH8511762699@users.noreply.github.com>
2023-10-03 18:06:19 +0530

    Initial commit

---
+++ pro1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("C is a general-purpose programming language that was originally\n developed in the early 1970s by Dennis Ritchie at Bell Labs.\n It is a widely used and influential programming language,\n often referred to as a middle-level language because it combines low-level\n features for efficient system programming with high-level constructs for application development.\n");
+
+    return 0;
+}
